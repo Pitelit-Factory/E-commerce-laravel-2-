4a8d1f08cdc7e676edc71a2f05e4d5b01df5c20d
Lempereur <lempereur.julien83@gmail.com>
2023-02-05 16:04:52 +0100

    commit 2

--- my-project/database/seeders/DatabaseSeeder.php
+++ my-project/database/seeders/DatabaseSeeder.php
@@ -20,7 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             userSeeder::class,
             produitSeeder::class,
-            // clientSeeder::class,
         ]);
     }
 }
